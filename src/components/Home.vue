413258b411d7fa50fb2a3b1685cbfaa6f5e4d03e
zhangleilei <zhangleilei@cnic.cn>
2020-08-04 12:24:46 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {}
+  },
+  props: {},
+  components: {},
+  watch: {},
+  computed: {},
+  methods: {
+    logout() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  },
+  created() {},
+  mounted() {}
+}
+</script>
+
+<style scoped lang="less">
+</style>
